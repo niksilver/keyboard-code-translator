1d1c5207b9f302b5b5c156100997762e022fe09c
Nik Silver <nik@niksilver.com>
2023-02-16 19:46:54 +0000

    Remove debugging; add more keys.

--- lib/mod.lua
+++ lib/mod.lua
@@ -16,7 +16,18 @@ k8 = {
   },
   -- Key codes from the keyboard we want to override
   overrides = {
+    [224] = 'F1'
+    [225] = 'F2'
+    [120] = 'F3'
     [204] = 'F4'
+    [229] = 'F5'
+    [230] = 'F6'
+    [165] = 'F7'
+    [164] = 'F8'
+    [163] = 'F9'
+    [113] = 'F10'
+    [114] = 'F11'
+    [115] = 'F12'
   }
 }
 
@@ -42,6 +53,7 @@ mod.hook.register("system_post_startup", "Keychron K8 post", function()
     -- `lua/core/keyboard.lua`.
 
     k8.revcodes = tab.invert(keyboard.codes)
+    i114] = 'F11'
   else
     print("No keyboard.codes")
   end
@@ -54,24 +66,18 @@ end)
 -- function if we're not in the mod menu.
 --
 function mod_keyboard_process(type, code, value)
-  print(type, code, value)
   k8.menu.values = {type, code, value, nil, nil}
   if code ~= nil then
     local want_name = k8.overrides[code]
     code = want_name and k8.revcodes[want_name] or code
     k8.menu.values[4] = want_name
     k8.menu.values[5] = code
-    print(want_name, code)
   end
 
   if k8.menu.is_in then
-    print("Redrawing with " .. tostring(mod_redraw))
     mod_redraw()
-    print("Redrawn")
   else
-    print("Entering " .. tostring(k8.original_keyboard_process))
     k8.original_keyboard_process(type, code, value)
-    print("Entered")
   end
 end
 
@@ -90,18 +96,15 @@ end
 
 m.enc = function(n, d) end
 
--- Show what might be sent to keyboard.process()
+-- Show what might be sent to keyboard.process(). We cannot call this
+-- redraw() because... of some reason which means it won't get called
+-- if it is.
 --
 function mod_redraw()
-  print("In redraw() 1")
-  for i = 1, 5 do
-    print("[" .. i .. "] = " .. tostring(k8.menu.values[i]))
-  end
   screen.clear()
   screen.move(0, 8); screen.text("Last keyboard message")
   screen.move(0, 16); screen.text("(type, code, value)")
   screen.move(0, 24); screen.text("and any translation:")
-  print("In redraw() 2")
 
   local trans = ""
   if k8.menu.values[4] then
@@ -110,24 +113,15 @@ function mod_redraw()
       " (" .. tostring(k8.menu.values[4]) ..
       ")"
   end
-  print("In redraw() 3")
 
   screen.move(0, 36); screen.text(
     "(" .. tostring(k8.menu.values[1]) ..
     ", " .. tostring(k8.menu.values[2]) ..
     ", " .. tostring(k8.menu.values[3]) ..
     ") " .. trans )
-  print("In redraw() 4")
+  screen.move(0, 60); screen.text("K2 to exit")
 
-  print(
-    "(" .. tostring(k8.menu.values[1]) ..
-    ", " .. tostring(k8.menu.values[2]) ..
-    ", " .. tostring(k8.menu.values[3]) ..
-    ") " .. trans )
-
-  print("In redraw() 5")
   screen.update()
-  print("In redraw() 6")
 end
 
 m.redraw = mod_redraw
@@ -146,33 +140,4 @@ end
 
 -- register the mod menu
 --
--- NOTE: `mod.this_name` is a convienence variable which will be set to the name
--- of the mod which is being loaded. in order for the menu to work it must be
--- registered with a name which matches the name of the mod in the dust folder.
---
 mod.menu.register(mod.this_name, m)
-
-
---
--- [optional] returning a value from the module allows the mod to provide
--- library functionality to scripts via the normal lua `require` function.
---
--- NOTE: it is important for scripts to use `require` to load mod functionality
--- instead of the norns specific `include` function. using `require` ensures
--- that only one copy of the mod is loaded. if a script were to use `include`
--- new copies of the menu, hook functions, and state would be loaded replacing
--- the previous registered functions/menu each time a script was run.
---
--- here we provide a single function which allows a script to get the mod's
--- state table. using this in a script would look like:
---
--- local mod = require 'name_of_mod/lib/mod'
--- local the_state = mod.get_state()
---
-local api = {}
-
-api.get_state = function()
-  return state
-end
-
-return api
